a27fb6b5e393950265c650f0a6675c486fe7c330
MondimTomas <mondimtomas@gmail.com>
2025-08-25 13:50:44 +0100

    Track MP4 via Git LFS; corrige BackgroundVideo.jsx; adiciona secções

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PortfolioPage from "./pages/PortfolioPage";
-import AboutPage from "./pages/AboutPage"; // 👈
+import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";   // 👈
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/portfolio" element={<PortfolioPage />} />
-      <Route path="/about" element={<AboutPage />} />   {/* 👈 nova rota */}
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/servicos" element={<ServicesPage />} /> {/* 👈 nova rota */}
     </Routes>
   );
 }
